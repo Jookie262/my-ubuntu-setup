420aebcf30c27f44c9f5ae608c0040784f6cc6e5
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-07-16 00:17:14 +0800

    Create Select Option Function and Fix Indentions

--- setup.sh
+++ setup.sh
@@ -4,22 +4,36 @@
 
 # Print the header design
 header () {
-    clear
-    figlet -f Big "Ubuntu Setup  "
-    echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n"
-    echo "    Created By     : Jookie262"
-    echo "    Github         : https://github.com/Jookie262/my-ubuntu-setup"
+  clear
+  figlet -f Standard " Ubuntu Setup  "
+  echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n"
+  echo -e "    Created By    : Jookie262"
+  echo -e "    Github        : https://github.com/Jookie262/my-ubuntu-setup" 
 }
 
-# Check if a certain package is installed
+# Checks if a certain package is installed
 # Install if not found otherwise print "Already Installed"
 checkPackage(){
- if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
- then
-   sudo apt-get install $1 -y;
- else
-   echo "Already Installed";
- fi
+  if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
+  then
+    sudo apt-get install $1 -y;
+  else
+    echo "Already Installed";
+  fi
+}
+
+# Ask user to select repeatedly until the user enters letter N
+selectOptions() {
+  echo ""
+  while true
+  do
+    read -p "  Select: " ans
+    
+    if [[ $ans == "N" ]] || [[ $ans == "n" ]]; then
+      return 1
+    fi
+
+  done
 }
 
 # ====================== End of Functions ====================== # 
@@ -36,5 +50,8 @@ checkPackage figlet
 
 # Call header function
 header
+
+# Call the selectOptions function
+selectOptions
 
 # ====================== End of Main ====================== # 
