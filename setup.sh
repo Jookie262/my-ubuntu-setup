498c7af03934121e3eab6b262175c0ecac7cee6d
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-07-15 17:17:08 +0800

    Organize Function and Insert New Codes
    Create a function for header design and function that checks if the package is install

--- setup.sh
+++ setup.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 
+# ====================== List of Functions ====================== # 
+
+# Print the header design
+header () {
+    clear
+    figlet -f Big "Ubuntu Setup  "
+    echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n"
+    echo "    Created By     : Jookie262"
+    echo "    Github         : https://github.com/Jookie262/my-ubuntu-setup"
+}
+
+# Check if a certain package is installed
+# Install if not found otherwise print "Already Installed"
+checkPackage(){
+ if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
+ then
+   sudo apt-get install $1 -y;
+ else
+   echo "Already Installed";
+ fi
+}
+
+# ====================== End of Functions ====================== # 
+
+
+
+# ====================== Start of Main ====================== # 
+
 # Update Repositories
 sudo apt update -y
 
-# Install Figlet for Art
-sudo apt install Figlet lolcat -y
+# Install Figlet for Header Art
+checkPackage figlet 
 
-clear
+# Call header function
+header
 
-figlet -f Big "Ubuntu Setup  " | lolcat
-echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n" | lolcat
-echo "    Created By     : Jookie262" | lolcat
-echo "    Github         : https://github.com/Jookie262/my-ubuntu-setup" | lolcat
+# ====================== End of Main ====================== # 
