7d236693df276cbd2b693ff9336b31a5864452df
Jookie262 <barrientos.134038@kalibo.sti.edu.ph>
2022-07-16 01:24:25 +0800

    Organize and Create New Methods
    Separate the checking and installing of packages, create a init package upon running the script and also create method for installing lolcat

--- setup.sh
+++ setup.sh
@@ -2,23 +2,51 @@
 
 # ====================== List of Functions ====================== # 
 
+# Needed Package upon running the setup.sh
+initPackages(){
+	installPackage figlet
+ 	installLolcat	
+}
+
 # Print the header design
 header () {
   clear
-  figlet -f Standard " Ubuntu Setup  "
-  echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n"
-  echo -e "    Created By    : Jookie262"
-  echo -e "    Github        : https://github.com/Jookie262/my-ubuntu-setup" 
+  figlet -f Standard " Ubuntu Setup  " | lolcat
+  echo -e "    Created a simple installer to automate downloading of\n            my applications and customizations\n" | lolcat
+  echo -e "    Created By    : Jookie262" | lolcat
+  echo -e "    Github        : https://github.com/Jookie262/my-ubuntu-setup" | lolcat 
 }
 
 # Checks if a certain package is installed
-# Install if not found otherwise print "Already Installed"
+# Returns yes or no
 checkPackage(){
-  if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
-  then
-    sudo apt-get install $1 -y;
-  else
-    echo "Already Installed";
+	if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+		isInstalled="no"
+	else
+		isInstalled="yes"
+	fi
+}
+
+# Install the package if the checkPackage method returns no
+installPackage(){
+  checkPackage $1
+  if [[ $isInstalled == "no" ]]; then
+    sudo apt-get install $1 -y
+  fi
+}
+
+# Install the packages needed for lolcat (for colorful text)
+installLolcat(){
+  # Install the lolcat package first
+  installPackage lolcat	
+  # Checks if the lolcat package is successfull
+  checkPackage lolcat
+  # If the lolcat package cannot be able to install then execute inside the if statement
+  if [[ $isInstalled == "no" ]]; then
+    installPackage gem
+    installPackage gcc
+    installPackage make
+    installPackage lolcat	    
   fi
 }
 
@@ -45,8 +73,8 @@ selectOptions() {
 # Update Repositories
 sudo apt update -y
 
-# Install Figlet for Header Art
-checkPackage figlet 
+# Call method that installs the needed packages
+initPackages
 
 # Call header function
 header
